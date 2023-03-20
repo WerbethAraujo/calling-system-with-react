7c6c053666ccc6067a6f835ad7d1cf6d7752e3a9
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-03-20 19:10:29 -0300

    refactor: delete context

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import SigIn from './pages/Sigin';
 import SigUp from './pages/Sigup';
-import Dashboard from './pages/Dashboard';
-
-import AuthProvider from './contexts/AuthContext';
-
-import PrivateRoute from './routes/PrivateRoute';
 
 function App() {
   return (
     <div className='app'>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<SigIn />} />
-            <Route path='/register' element={<SigUp />} />
-            <Route element={<PrivateRoute isPrivate={false} />}>
-              <Route path='/dashboard' element={<Dashboard />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SigIn />} />
+          <Route path='/register' element={<SigUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/routes/PrivateRoute.jsx
+++
@@ -1,5 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-export default function PrivateRoute({ isPrivate }) {
-  return isPrivate ? <Outlet /> : <Navigate to='/' />;
-}
